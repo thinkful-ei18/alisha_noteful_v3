c2b89c41cbf25bb55d56d5d86df1b36c3a3e0a51
alishaevn <alieva.inc@gmail.com>
2018-02-13 13:18:18 -0800

    can get a single note

--- routes/notes.js
+++ routes/notes.js
@@ -3,40 +3,37 @@
 const express = require('express');
 // Create an router instance (aka "mini-app")
 const router = express.Router();
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 
 const Note = require('../models/note');
 
 
 /* ========== GET/READ ALL ITEM ========== */
 router.get('/notes', (req, res, next) => {
-  console.log('abcd');
+
   const { searchTerm } = req.query;
   let filter = {};
 
   if (searchTerm) {
     const re = new RegExp(searchTerm, 'i');
-    // console.log('REGEX', re);
     filter.title = { $regex: re };
   }
-  console.log('FILTER', filter);
   return Note.find(filter)
-    // .select('title created')
-    // .sort('created')
+    .select('title created')
+    .sort('created')
     .then( note => {
-      console.log('NOTE', note);
       res.json(note);
     })
     .catch( err => next(err) );
 
 });
 
+
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/notes/:id', (req, res, next) => {
 
-  console.log('Get a Note');
-  res.json({ id: 2 });
+  Note.findById(req.params.id)
+    .then( note => res.json(note) )
+    .catch(err => next(err) );
 
 });
 
