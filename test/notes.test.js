d900e33d3a6025d395a53a1bd9dac72ac1d14ab3
alishaevn <alieva.inc@gmail.com>
2018-02-14 12:04:09 -0800

    required in all files and set up testing hooks

---
+++ test/notes.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const app = require('../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const chaiSpies = require('chai-spies');
+const seedNotes = require('../db/seed/notes.json');
+const mongoose = require('mongoose');
+const { TEST_MONGODB_URI } = require('../config.js');
+const expect = chai.expect;
+const Note = require('../models/note');
+
+
+chai.use(chaiHttp);
+chai.use(chaiSpies);
+
+describe('testing hooks', () => {
+  before(function() {
+    return mongoose.connect(TEST_MONGODB_URI, { autoIndex: false });
+  });
+
+  beforeEach(function () {
+    return Note.insertMany(seedNotes)
+      .then(() => Note.ensureIndexes());
+  });
+
+  afterEach(function () {
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function () {
+    return mongoose.disconnect();
+  });
+
+});
