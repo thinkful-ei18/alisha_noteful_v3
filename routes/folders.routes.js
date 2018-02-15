89fe89fd57ae0b2d7c5199b08f5d8b6d068c0f0c
alishaevn <alieva.inc@gmail.com>
2018-02-15 09:41:09 -0800

    created file, required necessities and outlined routes

---
+++ routes/folders.routes.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Note = require('../models/note.model');
+const Folder = require('../models/folder.model');
+
+
+
+/* ========== GET/READ ALL ITEM ========== */
+router.get('/folders', (req, res, next) => {
+
+
+
+});
+
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/folders/:id', (req, res, next) => {
+
+
+
+});
+
+
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/folders', (req, res, next) => {
+
+
+
+});
+
+
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/folders/:id', (req, res, next) => {
+
+
+
+});
+
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/folders/:id', (req, res, next) => {
+
+
+
+});
+
+module.exports = router;
