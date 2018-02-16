356ceb96aaafd58254d913d5027e6ea50cd6105a
alishaevn <alieva.inc@gmail.com>
2018-02-16 09:17:17 -0800

    created outline for tag routes

---
+++ routes/tags.routes.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const app = require('../server'); // first I must access the server
+const express = require('express'); // the server connects to my routes through Router, so I have to access express
+const router = express.Router(); // now I can use the router
+const mongoose = require('mongoose'); // and access my db
+
+const Tag = require('../models/tag.model'); // the collection I'll be referencing
+
+
+
+/* ========== GET/READ ALL ITEM ========== */
+router.get('/tags', (req, res, next) => {
+
+
+});
+
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/tags/:id', (req, res, next) => {
+
+
+});
+
+
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/tags', (req, res, next) => {
+
+
+});
+
+
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/tags/:id', (req, res, next) => {
+
+
+});
+
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/tags/:id', (req, res, next) => {
+
+
+});
+
+
+module.exports = router; // expose the routes
