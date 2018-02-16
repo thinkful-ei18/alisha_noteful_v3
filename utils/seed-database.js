e94f3f2d8a654575a9fc1b0cb21806976fb461aa
alishaevn <alieva.inc@gmail.com>
2018-02-16 08:58:47 -0800

    included tags in the seed db

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -6,42 +6,26 @@ mongoose.Promise = global.Promise;
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note.model');
 const Folder = require('../models/folder.model');
+const Tag = require('../models/tag.model');
+
 
 const seedNotes = require('../db/seed/notes');
 const seedFolders = require('../db/seed/folders');
+const seedTags = require('../db/seed/tags');
+
 
 
 mongoose.connect(MONGODB_URI)
+  .then( () =>  mongoose.connection.db.dropDatabase() )
   .then( () => {
-    return mongoose.connection.db.dropDatabase()
-      .then(result => {
-        console.info(`Dropped Database: ${result}`);
-      });
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(seedFolders),
+      Tag.insertMany(seedTags),
+      Note.createIndexes()
+    ]);
   })
-  .then(() => {
-    return Folder.insertMany(seedFolders)
-      .then(results => {
-        console.info(`Inserted ${results.length} Folders`);
-      });
-  })
-  .then(() => {
-    return Note.insertMany(seedNotes)
-      .then(results => {
-        console.info(`Inserted ${results.length} Notes`);
-      });
-  })
-  .then(() => {
-    return Note.createIndexes()
-      .then(() => {
-        console.info('Indexes created');
-      });
-  })
-  .then(() => {
-    return mongoose.disconnect()
-      .then(() => {
-        console.info('Disconnected');
-      });
-  })
+  .then( () => mongoose.disconnect() )
   .catch(err => {
     console.error(`ERROR: ${err.message}`);
     console.error(err);
