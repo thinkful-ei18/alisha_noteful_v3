939b68494a54f1b2736035c3a6a5b989051b4680
alishaevn <alieva.inc@gmail.com>
2018-02-20 13:29:31 -0800

    updated localStrategy to use .validatePassword

--- passport/local.js
+++ passport/local.js
@@ -5,8 +5,10 @@ const User = require('../models/user.model');
 
 const localStrategy = new LocalStrategy( (username, password, done) => {
  
+  let user;
   User.findOne({ username })
-    .then(user => {
+    .then(results => {
+      user = results;
       if (!user) {
         return Promise.reject({
           reason: 'LoginError',
@@ -14,7 +16,9 @@ const localStrategy = new LocalStrategy( (username, password, done) => {
           location: 'username'
         });
       }
-      const isValid = user.validatePassword(password);
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
       if (!isValid) {
         return Promise.reject({
           reason: 'LoginError',
